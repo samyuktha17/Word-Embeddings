8484df64452873f886e5cf9cc1aef9f17218fb3b
samyuktha17 <30373358+samyuktha17@users.noreply.github.com>
2020-03-29 19:24:06 -0700

    Update word2vec.py

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -2,23 +2,23 @@ import pandas as pd
 import nltk
 import gensim
 
-df = pd.read_csv("file_path\jokes.csv")
+df = pd.read_csv("file_path\jokes.csv") # load the file into a dataframe
 
-x = df["Question"].values.tolist()
+x = df["Question"].values.tolist() 
 y = df["Answer"].values.tolist()
 
-corpus = x + y
+corpus = x + y # create a corpus
 
-tok_corp= [nltk.word_tokenize(sentence) for sentence in corpus]
+tok_corp= [nltk.word_tokenize(sentence) for sentence in corpus] # tokenize the corpus
 
-model = gensim.models.Word2Vec(tok_corp, min_count = 1, size = 32)
+model = gensim.models.Word2Vec(tok_corp, min_count = 1, size = 32) # pass the tokenized corpus to the model ; consider every word that appears >=1 time ; size of the vector = 32
 
 model.save("testmodel")
 
 model = gensim.models.Word2Vec.load("testmodel")
 
-boys = model.most_similar("Boy")
-girls = model.most_similar("Girl")
+boys = model.most_similar("Boy") # find words that are associated with the word "Boy" 
+girls = model.most_similar("Girl") # find words that are associated with the word "Girl"
 
 print(boys)
 print(girls)
